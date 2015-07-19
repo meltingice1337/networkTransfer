a15f0b1b5d19147ab544f3e7a9d53b3b45fd1f04
BahNahNah <sam.mackenzie@live.com>
2015-07-20 01:03:33 +1200

    Init

---
+++ nTransfer_Client/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace nTransfer_Client
+{
+    class Program
+    {
+        private static eSock.Client _client = new eSock.Client();
+        static bool Connected = false;
+        private static FileStream _stream;
+        private static int FileHandle = 0;
+        static void Main(string[] args)
+        {
+            _client.OnDataRetrieved += _client_OnDataRetrieved;
+            _client.OnDisconnect += _client_OnDisconnect;
+            if (!_client.Connect("127.0.0.1", 1337))
+            {
+                Console.WriteLine("Failed to connect");
+                Console.ReadLine();
+                return;
+            }
+            Connected = true;
+            
+            while (Connected)
+            {
+                try
+                {
+                    Console.Write("File to send: ");
+                    string path = Console.ReadLine();
+                    _stream = new FileStream(path, FileMode.Open);
+                    _client.Send((byte)PacketHeader.StartFileTransfer, Path.GetFileName(path));
+                    Console.WriteLine("Waiting for responce..");
+                    while(FileHandle == 0)
+                    {
+                        Thread.Sleep(100);
+                    }
+                    Console.WriteLine("Handle: {0}", FileHandle);
+                    byte[] buffer = new byte[1000];
+                    int bytesRead = 0;
+                    while ((bytesRead = _stream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        byte[] block = new byte[bytesRead];
+                        Buffer.BlockCopy(buffer, 0, block, 0, block.Length);
+                        _client.Send((byte)PacketHeader.FileTransferBlock, FileHandle, block, _stream.Position == _stream.Length);
+                        Console.WriteLine("Sent block ({0} bytes) || LastBlock: {1}", bytesRead, _stream.Position == _stream.Length);
+                    }
+                    FileHandle = 0;
+                    _stream.Dispose();
+                    _stream = null;
+                    Console.WriteLine("File Complete");
+                }
+                catch
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+            Console.WriteLine("Not connected");
+            Console.ReadLine();
+        }
+
+        static void _client_OnDisconnect(eSock.Client sender, System.Net.Sockets.SocketError ER)
+        {
+            Connected = false;
+        }
+
+        static void _client_OnDataRetrieved(eSock.Client sender, object[] data)
+        {
+            byte header = (byte) data[0];
+            if (header == (byte)PacketHeader.FileTransferResponce)
+            {
+                FileHandle = (int)data[1];
+            }
+        }
+    }
+}
---
+++ nTransfer_Server/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nTransfer_Server
+{
+    class Program
+    {
+        private static eSock.Server _server = new eSock.Server();
+        static Dictionary<int, FileStream> FileHandles = new Dictionary<int, FileStream>();
+        private static int Handle = 1;
+        static void Main(string[] args)
+        {
+            _server.OnClientConnect += _server_OnClientConnect;
+            _server.OnDataRetrieved += _server_OnDataRetrieved;
+            _server.OnClientDisconnect += _server_OnClientDisconnect;
+            _server.Start(1337);
+            Console.WriteLine("Started on 1337");
+            while (true)
+            {
+                Console.ReadLine();
+            }
+        }
+
+        static void _server_OnClientDisconnect(eSock.Server sender, eSock.Server.eSockClient client, System.Net.Sockets.SocketError ER)
+        {
+            Console.WriteLine("Client disconnected");
+        }
+
+        static void _server_OnDataRetrieved(eSock.Server sender, eSock.Server.eSockClient client, object[] data)
+        {
+            PacketHeader header = (PacketHeader) (byte)data[0];
+            if (header == PacketHeader.StartFileTransfer)
+            {
+                int _fHandle = Handle;
+                lock (_server)
+                {
+                    while (FileHandles.ContainsKey(_fHandle))
+                    {
+                        _fHandle = ++Handle;
+                    }
+                }
+                FileHandles.Add(_fHandle, new FileStream((string)data[1], FileMode.Create));
+                client.Send((byte)PacketHeader.FileTransferResponce, _fHandle);
+            }
+
+            if (header == PacketHeader.FileTransferBlock)
+            {
+                int _fhandle = (int) data[1];
+                byte[] block = (byte[]) data[2];
+                bool finalBlock = (bool) data[3];
+                if (!FileHandles.ContainsKey(_fhandle))
+                    return;
+                FileHandles[_fhandle].Write(block, 0, block.Length);
+                if (finalBlock)
+                {
+                    FileHandles[_fhandle].Close();
+                    FileHandles[_fhandle].Dispose();
+                    lock (_server)
+                    {
+                        if (Handle > _fhandle)
+                            Handle = _fhandle;
+                    }
+                }
+                    
+            }
+        }
+
+        static void _server_OnClientConnect(eSock.Server sender, eSock.Server.eSockClient client)
+        {
+            
+                 Console.WriteLine("Client connected");
+        }
+    }
+}
